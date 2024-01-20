b5b86ce0abb97e280a40dd8c3fc098aceea82d67
Thanayut T <b@thanayut.in.th>
2024-01-20 12:04:30 +0700

    Setup simple import sort

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,6 +1,7 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
 import React from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
 
 const app = document.getElementById("root") as HTMLDivElement;
 
